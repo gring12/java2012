771e934317d43d51c4ef0b528b28dbb832f96ea4
gring12 <76715473+gring12@users.noreply.github.com>
2020-12-30 10:39:48 +0900

    2020-12-30 10:39

--- Lesson001/src/com/swcodingschool/hello/QuizConditionalStatement.java
+++ Lesson001/src/com/swcodingschool/hello/QuizConditionalStatement.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class QuizConditionalStatement {
 
 	public static void main(String[] args) {
-		int score = 0;
-		
+				
 		Scanner sc = new Scanner(System.in);
 		System.out.println("score를 입력하세요.");
-		int scoreOfStudent = sc.nextInt();
+		int score = sc.nextInt();
 				
 		if(score>=90) {
 			System.out.println(score + "는 A등급입니다.");
