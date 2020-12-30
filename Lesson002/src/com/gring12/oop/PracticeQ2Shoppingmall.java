4759e64a8b93d3e9ff0f35a416c0f220613ce2a8
gring12 <76715473+gring12@users.noreply.github.com>
2020-12-31 01:13:51 +0900

    2020-12-31 1:13

--- Lesson002/src/com/gring12/oop/PracticeQ2Shoppingmall.java
+++ Lesson002/src/com/gring12/oop/PracticeQ2Shoppingmall.java
@@ -3,7 +3,7 @@ package com.gring12.oop;
 public class PracticeQ2Shoppingmall {
 	long number;
 	String ID;
-	int date;
+	String date;
 	String name;
 	String numberOfProduct;
 	String address;
@@ -12,17 +12,17 @@ public class PracticeQ2Shoppingmall {
 		PracticeQ2Shoppingmall Order = new PracticeQ2Shoppingmall ();
 			Order.number = 201803120001L ;
 			Order.ID = "abc123" ;
-			Order.date = 20180312 ;
+			Order.date = "2018년 03월 12일" ;
 			Order.name = "홍길순" ;
 			Order.numberOfProduct = "PD0345-12" ;
 			Order.address = "서울시 영등포구 여의도동 20번지" ;
 			
 		System.out.println("주문 번호 : " + Order.number);
 		System.out.println("주문자 아이디 : " + Order.ID);
-		System.out.println("주문 날짜" + Order.date);
-		System.out.println("주문자 이름" + Order.name);
-		System.out.println("주문 상품 번호" + Order.numberOfProduct);
-		System.out.println("배송 주소" + Order.address);
+		System.out.println("주문 날짜 : " + Order.date);
+		System.out.println("주문자 이름 : " + Order.name);
+		System.out.println("주문 상품 번호 : " + Order.numberOfProduct);
+		System.out.println("배송 주소 : " + Order.address);
 			
 	}
 
