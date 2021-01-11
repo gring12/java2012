dcc5cd878aae99c7cd295ccfbd5d65fc83c702a3
gring12 <76715473+gring12@users.noreply.github.com>
2021-01-11 10:46:02 +0900

    2021-01-11 10:45

--- Lesson003/src/com/gring12/guibasic/Login.java
+++ Lesson003/src/com/gring12/guibasic/Login.java
@@ -31,8 +31,9 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					// 데이터베이스 연결
-					if(DBUtil.dbconn == null)DBUtil.DBConnect();
-					
+					if (DBUtil.dbconn == null)
+						DBUtil.DBConnect();
+
 					Login frame = new Login();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -53,62 +54,62 @@ public class Login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Simple Login Demo");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel.setBounds(106, 10, 221, 43);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("USER ID");
 		lblNewLabel_1.setBounds(59, 92, 57, 15);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("PASSWORD");
 		lblNewLabel_2.setBounds(59, 139, 72, 15);
 		contentPane.add(lblNewLabel_2);
-		
+
 		txtUserID = new JTextField();
 		txtUserID.setBounds(163, 89, 164, 21);
 		contentPane.add(txtUserID);
 		txtUserID.setColumns(20);
-		
+
 		txtUserPWD = new JPasswordField();
 		txtUserPWD.setColumns(45);
 		txtUserPWD.setBounds(163, 136, 164, 21);
 		contentPane.add(txtUserPWD);
-		
+
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// 데이터베이스 연결
-				if(DBUtil.dbconn == null)DBUtil.DBConnect();
+				if (DBUtil.dbconn == null)
+					DBUtil.DBConnect();
 				// 입력된 유저 아이디와 비밀번호를 가져온다.
 				String userid = txtUserID.getText();
 				String userpwd = txtUserPWD.getText();
-				//입력된 아이디와 비번 확인
+				// 입력된 아이디와 비번 확인
 //				System.out.println(userid);
 //				System.out.println(userpwd);
-				
-			
+
 				// 유저아이디와 비밀번호를 사용하여 데이터베이스 조회
-				/* SELECT 속성리스트[* all, 모든 속성]
-				   FROM 테이블이름
-				   WHERE 조건 */
+				/*
+				 * SELECT 속성리스트[* all, 모든 속성] FROM 테이블이름 WHERE 조건
+				 */
 				// sql 구문 구성
 				String sql = "SELECT * FROM tbluser WHERE userid=? AND userpwd=?";
-			
+
 				try {
 					// 유저 아이디와 비밀번호를 사용하여 이 유저 아이디와 비밀번호로 DB 연결 시도
 					PreparedStatement pstmt = DBUtil.dbconn.prepareStatement(sql);
 					pstmt.setString(1, userid); // userid 변수값으로 sql구문의 첫번째 ? 에 대입
 					pstmt.setString(2, userpwd);// userpwd 변수값으로 sql구문의 두번째 ? 에 대입
-				
+
 					// 최종 완성된 질의구문을 실행하고 그 결과를 ResultSet으로 받아온다.
 					ResultSet rs = pstmt.executeQuery();
-				
+
 					// resultset rs내에는 검색된 결과값들이 들어있는데...
-					if(rs.next()) { // 해당 계정이 있으면, 정상 로그인
+					if (rs.next()) { // 해당 계정이 있으면, 정상 로그인
 						rs.close();
 						pstmt.close();
 						// System.out.println("로그인성공");
@@ -116,16 +117,16 @@ public class Login extends JFrame {
 						dispose();
 						AppHome apphome = new AppHome(); // 새 프레임 생성
 						apphome.setVisible(true); // 프레임이 보이도록 한다.
-					}else {// 해당 계정이 없음
+					} else {// 해당 계정이 없음
 						System.out.println("아이디와 비번을 다시 확인해주세요.");
 					}
-					}catch(SQLException elogin) {
-						System.out.println("[MyMSG]SQL Exception Error : " + elogin.getMessage());
-						elogin.printStackTrace();
-					}
+				} catch (SQLException elogin) {
+					System.out.println("[MyMSG]SQL Exception Error : " + elogin.getMessage());
+					elogin.printStackTrace();
+				}
 			}
 		});
-			btnLogin.setBounds(168, 195, 97, 23);
-			contentPane.add(btnLogin);
+		btnLogin.setBounds(168, 195, 97, 23);
+		contentPane.add(btnLogin);
 	}
 }
