8ef38ebeb93aff9fe4ca909af7ea80207bcdb4fa
gring12 <76715473+gring12@users.noreply.github.com>
2021-01-27 16:47:37 +0900

    2021-01-27 16:47

---
+++ Lesson240/src/main/java/com/example/Lesson240/AccessMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lesson240;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessMysqlApplication.class, args);
+	}
+
+}
---
+++ Lesson240/src/test/java/com/example/Lesson240/AccessMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lesson240;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
