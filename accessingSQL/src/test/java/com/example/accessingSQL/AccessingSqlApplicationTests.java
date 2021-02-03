ae8ae1ad63a0cb05a64255c3a66481aff893a292
gring12 <76715473+gring12@users.noreply.github.com>
2021-02-03 11:47:09 +0900

    2021-02-03 11:47

---
+++ accessingSQL/src/test/java/com/example/accessingSQL/AccessingSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
