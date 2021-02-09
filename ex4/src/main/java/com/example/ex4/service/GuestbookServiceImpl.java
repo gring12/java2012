578f07836d064420022392fb9b504dc1fb6a0194
gring12 <76715473+gring12@users.noreply.github.com>
2021-02-09 16:50:42 +0900

    2021-02-09 16:50

---
+++ ex4/src/main/java/com/example/ex4/service/GuestbookServiceImpl.java
@@ -0,0 +1,57 @@
+package com.example.ex4.service;
+
+import com.example.ex4.dto.GuestbookDTO;
+import com.example.ex4.dto.PageRequestDTO;
+import com.example.ex4.dto.PageResultDTO;
+import com.example.ex4.entity.Guestbook;
+import com.example.ex4.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository;
+
+    @Override
+    public Long register(GuestbookDTO dto){
+
+        log.info("DTO--------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO){
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDTO(entity));
+
+        return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno){
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent()? entityToDTO(result.get()): null;
+    }
+}
