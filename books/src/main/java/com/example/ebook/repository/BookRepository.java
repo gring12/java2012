7413a61b5a90caea0605b27ac2852e72906f2792
gring12 <76715473+gring12@users.noreply.github.com>
2021-02-24 16:20:20 +0900

    2021-02-24 16:20

--- books/src/main/java/com/example/ebook/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ebook.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ebook.entity.Book;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface BookRepository extends JpaRepository<Book, Integer>, QuerydslPredicateExecutor<Book> {
-
-}
